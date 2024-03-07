6a6e9d1214478b7cafa4962e48af463f33e757f1
Limiana <5073202+Limiana@users.noreply.github.com>
2024-03-07 05:38:20 +0300

    it will do for now, will revisit later

--- LabelWare/Program.cs
+++ LabelWare/Program.cs
@@ -1,11 +1,44 @@
 ﻿using Octokit;
+using System.Collections.Immutable;
 
 namespace LabelWare;
 
 internal class Program
 {
+    public static GithubManager GithubManager = new();
+    public static ImmutableList<LabelConfiguration> Configuration = [];
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"Welcome to LabelWare by Limiana. At any time, type \"quit\" to exit current operation. ");
+        Configuration = Utils.LoadLabelConfiguration();
+        while (true)
+        {
+            Console.WriteLine("> Available commands:");
+            Console.WriteLine("  > token [your token] - sets your Personal access token");
+            Console.WriteLine("  > reload - reloads configuration files");
+            Console.WriteLine("  > dry - performs dry run and displays what changes would have been made");
+            Console.WriteLine("  > wet - performs changes according to loaded configuration");
+            var command = Console.ReadLine() ?? "";
+            if (command == "quit") Environment.Exit(0);
+            if(command.StartsWith("token "))
+            {
+                GithubManager.PAT = command.Split(" ")[1];
+                Console.WriteLine("Token set.");
+            }
+            if(command == "reload")
+            {
+                Configuration = Utils.LoadLabelConfiguration();
+            }
+            if (command == "dry")
+            {
+                var task = GithubManager.Run(false);
+                task.Wait();
+            }
+            if (command == "wet")
+            {
+                var task = GithubManager.Run(true);
+                task.Wait();
+            }
+        }
     }
 }
---
+++ LabelWare/Utils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelWare;
+public static class Utils
+{
+    public static ImmutableList<LabelConfiguration> LoadLabelConfiguration()
+    {
+        var ret = new List<LabelConfiguration>();
+        try
+        {
+            var lst = File.ReadAllText("labels.csv").ReplaceLineEndings().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if(lst.Length < 2)
+            {
+                Console.WriteLine($"Please populate labels.csv");
+            }
+            var separator = ",";
+            for (int i = 1; i < lst.Length; i++)
+            {
+                var array = lst[i].Split(separator);
+                if (array.Length < 4) continue; 
+                var entry = new LabelConfiguration(array[0], array[1], array[2], array[3..]);
+                ret.Add(entry);
+                Console.WriteLine($"Loaded {entry}");
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        return ret.ToImmutableList();
+    }
+
+    /// <summary>
+    /// Safely selects an entry of the list at a specified index, returning default value if index is out of range.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public static T SafeSelect<T>(this IList<T> list, int index)
+    {
+        if (index < 0 || index >= list.Count) return default;
+        return list[index];
+    }
+
+    /// <summary>
+    /// Safely selects an entry of the array at a specified index, returning default value if index is out of range.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public static T SafeSelect<T>(this T[] list, int index)
+    {
+        if (index < 0 || index >= list.Length) return default;
+        return list[index];
+    }
+}
